44230b6de52be6eb394199db14bb23ba13eef346
GreenhairTurtle <912967642@qq.com>
2021-09-25 18:53:39 +0800

    Fix ATA bug

--- LibGetFrame-1.0.lua
+++ LibGetFrame-1.0.lua
@@ -1,5 +1,5 @@
 local MAJOR_VERSION = "LibGetFrame-1.0"
-local MINOR_VERSION = 28
+local MINOR_VERSION = 29
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
 local lib = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if not lib then return end
@@ -30,7 +30,7 @@ local defaultFramePriorities = {
     "^LimeGroup", -- lime
     "^SUFHeaderraid", -- suf
     "^LUFHeaderraid", -- luf
-    "^AshToAsh", -- AshToAsh
+    "^AshToAshUnit%d+Unit%d+", -- AshToAsh
     -- party frames
     "^AleaUI_GroupHeader", -- Alea
     "^SUFHeaderparty", --suf
@@ -82,7 +82,7 @@ local defaultPartyFrames = {
     "^ElvUF_PartyGroup",
     "^oUF_.-Party",
     "^PitBull4_Groups_Party",
-    "^AshToAsh",
+    "^AshToAshUnit%d+Unit%d+",
     "^CompactParty",
 }
 local defaultPartyTargetFrames = {
@@ -101,7 +101,7 @@ local defaultRaidFrames = {
     "^PlexusLayout",
     "^ElvUF_RaidGroup",
     "^oUF_.-Raid",
-    "^AshToAsh",
+    "^AshToAshUnit%d+Unit%d+",
     "^LimeGroup",
     "^SUFHeaderraid",
     "^LUFHeaderraid",
